99978e5691f12c0ce184cc678891ebd0aa4768cb
Ben Switz <ben@codinghaze.com>
2022-06-17 11:14:13 -0500

    Added grass image Worked on Hero

--- cut-n-cruise/src/components/Hero.vue
+++ cut-n-cruise/src/components/Hero.vue
@@ -1,6 +1,12 @@
 <script>
 </script>
 <template>
+<div class="HeroCont">
+<img class="w-full top-56 object-fill" src="../assets/grass.png">
+</div>
 </template>
-<style>
+<style> 
+.HeroCont{
+    @apply border border-black w-full overflow-hidden  h-96;
+}
 </style>
