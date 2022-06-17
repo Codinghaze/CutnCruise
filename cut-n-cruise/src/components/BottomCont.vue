ef2753b87067c37fc9ec0caeb480d95f39294263
Ben Switz <ben@codinghaze.com>
2022-06-17 16:35:52 -0500

    Added contact us and bottom content boxes

---
+++ cut-n-cruise/src/components/BottomCont.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<!-- This contains the textbox and title -->
+<template>
+<div class="ContentBox">Acceptional Work at Affordable Costs</div>
+</template>
+<style scoped> 
+.ContentBox{
+    @apply flex-1 h-96 border border-green-600 text-2xl font-bold text-center;
+}
+</style>
