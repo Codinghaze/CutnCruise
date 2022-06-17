1a771435710619868642e7ea7c43a8e3178e4918
Ben Switz <ben@codinghaze.com>
2022-06-17 11:13:30 -0500

    Created header set up hero
    Co-authored-by: Codinghaze <Codinghaze@users.noreply.github.com>

---
+++ cut-n-cruise/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from './components/Header.vue';
+import "./tailwind.css";
+import Hero from './components/Hero.vue';
+</script>
+
+<template>
+  <Header />
+  <Hero></Hero>
+</template>
+
+<style>
+
+</style>
---
+++ cut-n-cruise/src/components/Header.vue
@@ -0,0 +1,21 @@
+<script>
+</script>
+<template>
+    <div class="headerRow">
+    <div class="titlebox">Cut n Cruise</div>
+    <div class="linkBoxes">Home</div>
+        <div class="linkBoxes">About</div>
+        <div class="linkBoxes">Services</div>
+    </div>
+</template>
+<style>
+    .headerRow {
+        @apply w-full flex flex-row items-center justify-start bg-green-500 h-24 px-5 py-2;
+    }
+    .linkBoxes {
+        @apply w-48 flex flex-row items-center justify-center text-3xl hover:underline  h-full px-1 py-1;
+    }
+    .titlebox{
+        @apply flex-1 flex flex-row items-center justify-start text-5xl font-bold;
+    }
+</style>
