59ae5f0bebeb3e1a1a0efecc2e4948d65bfe2966
Ben Switz <ben@codinghaze.com>
2022-06-23 12:20:24 -0500

    Co-authored-by: Codinghaze <Codinghaze@users.noreply.github.com>

--- cut-n-cruise/src/components/ContactUs.vue
+++ cut-n-cruise/src/components/ContactUs.vue
@@ -2,82 +2,93 @@
 export default {
   data() {
     return {
-        phone: "",
-        email: "",
-        name: "",
-    }
+      phone: "",
+      email: "",
+      name: "",
+    };
   },
   methods: {
     CheckEmail() {
-        if(this.email.includes("@") )
-        {
-            return true
-        }
-        return false   
+      if (this.email.includes("@")) {
+        return true;
+      }
+      return false;
     },
-    CheckPhoneLength(){
-        if(this.phone.length == 10 )
-        {
-            return true
-        }
-        return false   
+    CheckPhoneLength() {
+      if (this.phone.length == 10) {
+        return true;
+      }
+      return false;
     },
     CheckForSpaceinName() {
-         if(this.name.includes(" ") )
-        {
-            return true
-        }
-        return false  
+      if (this.name.includes(" ")) {
+        return true;
+      }
+      return false;
     },
-    SubmitBtn(){
-       if(this.CheckPhoneLength() == false){
-        alert("Phone Number too short")
-       }
-
-       if(this.CheckEmail() == false){
-        alert("Bad Email")
-       }
-
-        if(this.CheckForSpaceinName() == false){
-        alert("Please enter first and last name")
-       }
-    }
-  }
-}
+    async SubmitBtn() {
+      try {
+        if (this.CheckPhoneLength() == false) {
+          alert("Phone Number too short");
+        } else if (this.CheckEmail() == false) {
+          alert("Bad Email");
+        } else if (this.CheckForSpaceinName() == false) {
+          alert("Please enter first and last name");
+        } else {
+          await fetch(
+            `http://159.65.172.156:3000/?name=${this.name}&phone=${this.phone}`
+          );
+          
+        alert("Request Submitted! A contact will reachout within 24 hours");
+        }
+      } catch (e) {
+        alert("An error has occured");
+      }
+    },
+  },
+};
 </script>
 
 <!-- This contains the textbox and title -->
 <template>
-<div class="ContactUs">
-    Contact Us 
+  <div class="ContactUs">
+    Contact Us
     <div class="InputBox">
-        <div class="HalfInput">Name:</div>
-        <div class="HalfInput"> <input v-model="name" class="TextBox" /></div>
-        <!-- title div contains: name of field -->
-        <!-- input div contain: actual input -->
+      <div class="HalfInput">Name:</div>
+      <div class="HalfInput"><input v-model="name" class="TextBox" /></div>
+      <!-- title div contains: name of field -->
+      <!-- input div contain: actual input -->
     </div>
-    <div class="InputBox"> 
-        <div class="HalfInput">Email:</div>
-        <div class="HalfInput"> <input class="TextBox" v-model="email" /></div></div>
     <div class="InputBox">
-         <div class="HalfInput">Phone:</div>
-        <div class="HalfInput"> <input class="TextBox" v-model="phone" /></div></div>
-    
-    <div class="InputBox"><button @click="SubmitBtn" class="w-36 hover:bg-green-500 active:bg-green-400 rounded-xl h-full bg-green-700 text-white "> Submit</button></div>
-</div>
+      <div class="HalfInput">Email:</div>
+      <div class="HalfInput"><input class="TextBox" v-model="email" /></div>
+    </div>
+    <div class="InputBox">
+      <div class="HalfInput">Phone:</div>
+      <div class="HalfInput"><input class="TextBox" v-model="phone" /></div>
+    </div>
+
+    <div class="InputBox">
+      <button
+        @click="SubmitBtn"
+        class="w-36 hover:bg-green-500 active:bg-green-400 rounded-xl h-full bg-green-700 text-white"
+      >
+        Submit
+      </button>
+    </div>
+  </div>
 </template>
-<style scoped> 
-.ContactUs{
-    @apply w-96 h-96 border p-4 border-black flex flex-col items-center text-center justify-start text-3xl font-bold ;
+<style scoped>
+.ContactUs {
+  @apply w-96 h-96 border p-4 border-black flex flex-col items-center text-center justify-start text-3xl font-bold;
 }
-.InputBox{
-    @apply w-full h-12 border my-2 border-black flex flex-row items-center justify-center;
+.InputBox {
+  @apply w-full h-12  my-2 border-black flex flex-row items-center justify-center;
 }
 .HalfInput {
-@apply w-1/2 border border-black h-full text-lg flex flex-row items-center justify-center;
+  @apply w-1/2 border border-black h-full text-lg flex flex-row items-center justify-center;
 }
 .TextBox {
-    @apply w-full h-full p-4
-    
+  @apply w-full h-full p-4;
 }
-</style> 
+</style>
--- cut-n-cruise/src/components/Header.vue
+++ cut-n-cruise/src/components/Header.vue
@@ -13,7 +13,7 @@
         @apply w-full flex flex-row items-center justify-start bg-green-500 h-24 px-5 py-2;
     }
     .linkBoxes {
-        @apply w-48 flex flex-row items-center justify-center text-3xl hover:underline  h-full px-1 py-1;
+        @apply w-48 flex flex-row items-center justify-center text-3xl hover:underline select-none h-full px-1 py-1;
     }
     .titlebox{
         @apply flex-1 flex flex-row items-center justify-start text-5xl font-bold;
